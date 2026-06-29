aac6f08276afcc3591c4fc3c0986468136f457e9
ThatBlueEnder <ThatBlueEnder@gmail.com>
2026-06-28 23:16:43 -0400

    Append

---
+++ script.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        Youtube Mobile
+// @match       *://m.youtube.com/*
+// @run-at      document-start
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  const pushState = history.pushState;
+
+  history.pushState = function (...args) {
+
+    const url = new URL(args[2], location.href);
+
+    if (url.searchParams.has("start_radio")) {
+      url.searchParams.delete("start_radio");
+      url.searchParams.delete("list");
+      location.replace(url.href);
+    }
+
+    return pushState.apply(this, args);
+  };
+
+
+})();
